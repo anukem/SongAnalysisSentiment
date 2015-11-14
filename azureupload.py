1b86e50c0a448a77b2cbe0af112017e126e40251
tbeiko <t.beiko23@gmail.com>
2015-11-14 08:55:40 -0500

    change naming of mp3s

--- azureupload.py
+++ azureupload.py
@@ -4,7 +4,7 @@ import os
 blob_service = BlobService(account_name='songanalysis', account_key=os.environ['AZUREKEY'])
 blob_service.put_block_blob_from_path(
     'songs',
-    'happy',
-    'happy.mp3',
+    '10_happy',
+    '10_happy.mp3',
     x_ms_blob_content_type='mp3'
 )
