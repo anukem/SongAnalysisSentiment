573d643df1799e05454b5538692b520a65366a81
tbeiko <t.beiko23@gmail.com>
2015-11-14 03:18:07 -0500

    refactor songdicationary into functions

--- azureupload.py
+++ azureupload.py
@@ -1,1 +1,10 @@
 from azure.storage.blob import BlobService
+import os
+
+blob_service = BlobService(account_name='songanalysis', account_key=os.environ['AZUREKEY'])
+blob_service.put_block_blob_from_path(
+    'songs',
+    'happy',
+    'happy.mp3',
+    x_ms_blob_content_type='mp3'
+)
--- songdictionary.py
+++ songdictionary.py
@@ -2,42 +2,46 @@
 # Will need to get the value from the sentiment analysis for this
 import random
 value = random.uniform(-1.0, 1) 
-
 print value
+
 # Use a switch statement to correlate that value to our code
-if    ((-1.0 <= value) & (value < -0.8)):
-        sentValue = -1.0
-elif  ((-0.8 <= value) & (value < -0.6)):
-        sentValue = -0.8
-elif  ((-0.6 <= value) & (value < -0.4)):
-        sentValue = -0.6
-elif  ((-0.4 <= value) & (value < -0.2)):
-        sentValue = -0.4
-elif  ((-0.2 <= value) & (value <  0.0)):
-        sentValue = -0.2
-elif  (( 0.0 <= value) & (value <  0.2)):
-        sentValue =  0.0
-elif  (( 0.2 <= value) & (value <  0.4)):
-        sentValue =  0.2
-elif  (( 0.4 <= value) & (value <  0.6)):
-        sentValue =  0.4
-elif  (( 0.6 <= value) & (value <  0.8)):
-        sentValue =  0.6
-elif  (( 0.8 <= value) & (value <  1.0)):
-        sentValue =  0.8
-else: 
-        sentValue =  1.0                    # Happy - Pharell
+def getSentValue(value):
+        if    ((-1.0 <= value) & (value < -0.8)):
+                sentValue = -1.0
+        elif  ((-0.8 <= value) & (value < -0.6)):
+                sentValue = -0.8
+        elif  ((-0.6 <= value) & (value < -0.4)):
+                sentValue = -0.6
+        elif  ((-0.4 <= value) & (value < -0.2)):
+                sentValue = -0.4
+        elif  ((-0.2 <= value) & (value <  0.0)):
+                sentValue = -0.2
+        elif  (( 0.0 <= value) & (value <  0.2)):
+                sentValue =  0.0
+        elif  (( 0.2 <= value) & (value <  0.4)):
+                sentValue =  0.2
+        elif  (( 0.4 <= value) & (value <  0.6)):
+                sentValue =  0.4
+        elif  (( 0.6 <= value) & (value <  0.8)):
+                sentValue =  0.6
+        else:
+                sentValue =  1.0
 
+        return sentValue
 
-dict = {-1.0 : 'saddest song', 
-        -0.8 : 'sadder song',
-        -0.6 : 'saddish song',
-        -0.4 : 'saddy song',
-        -0.2 : 'sad song', 
-         0.0 : 'okay song', 
-         0.2 : 'okayish song',
-         0.4 : 'goodish song',
-         0.6 : 'good song',
-         0.8 : 'happiysh song',
-         1.0 : 'happy song'}
-print dict[sentValue]
+def getSongUrlFromValue(value):
+        sentValue = getSentValue(value)
+        songdisc = {-1.0 : 'saddest song', 
+                -0.8 : 'sadder song',
+                -0.6 : 'saddish song',
+                -0.4 : 'saddy song',
+                -0.2 : 'sad song', 
+                 0.0 : 'okay song', 
+                 0.2 : 'okayish song',
+                 0.4 : 'goodish song',
+                 0.6 : 'good song',
+                 0.8 : 'happiysh song',
+                 1.0 : 'happy song'}
+        return songdisc[sentValue]
+
+print getSongUrlFromValue(value)
